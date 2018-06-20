68a4dc741d53330310e1c169a0a0c49c62013d3e
mfboulos <mboulos95@gmail.com>
2018-06-20 15:15:40 -0400

    hi again

--- src/main/java/com/rev/servlet/TestServlet.java
+++ src/main/java/com/rev/servlet/TestServlet.java
@@ -1,6 +1,8 @@
 package com.rev.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,9 +14,24 @@ import javax.servlet.http.HttpServletResponse;
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	private boolean isPrime(int n) {
+		if(n == 1)
+			return false;
+		for(int i = 2; i <= Math.sqrt((double) n); i++) {
+			if(n%i == 0)
+				return false;
+		}
+		return true;
+	}
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter pw = response.getWriter();
+		pw.append("Prime numbers from 2 to 1000000:\n\n");
+		
+		for(int i = 1; i < 1000000; i++) {
+			if(isPrime(i))
+				pw.append(i + "  ");
+		}
 	}
 
 	/**
